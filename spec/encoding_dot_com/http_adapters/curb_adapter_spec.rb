b41d0ab25090d851fad17db97c652f45844fb032
Alex O'Rielly <aorielly@qualvu.com>
2013-05-03 10:53:08 -0600

    replaced depricated rspec function in the curb spec

--- spec/encoding_dot_com/http_adapters/curb_adapter_spec.rb
+++ spec/encoding_dot_com/http_adapters/curb_adapter_spec.rb
@@ -3,15 +3,15 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 describe "CurbAdapter" do
   before :each do
     @http = EncodingDotCom::HttpAdapters::CurbAdapter.new
-    @mock_easy = mock("Curl::Easy mock", :null_object => true)
-    @mock_easy.should_receive(:follow_location=).with(true)    
-    Curl::Easy.should_receive(:new).with("http://example.com").and_yield(@mock_easy).and_return(@mock_easy)    
+    @mock_easy = mock("Curl::Easy mock").as_null_object
+    @mock_easy.should_receive(:follow_location=).with(true)
+    Curl::Easy.should_receive(:new).with("http://example.com").and_yield(@mock_easy).and_return(@mock_easy)
   end
 
   it "should respond to post with a url and hash of post parameters" do
     @mock_easy.should_receive(:http_post)
     Curl::PostField.should_receive(:content).with("foo", "bar")
-    
+
     @http.post("http://example.com", {:foo => "bar"})
   end
 
