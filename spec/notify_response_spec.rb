51c6e88e9ee356df20ca786238271f04e5517f5e
Jerel Miller <jmiller@24tru.com>
2013-06-25 12:50:21 -0600

    Fixing syntax

--- spec/notify_response_spec.rb
+++ spec/notify_response_spec.rb
@@ -1,4 +1,4 @@
-  require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Encoding.com notify response parser" do
 
